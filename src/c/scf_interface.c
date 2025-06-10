6488e7f2037c4c5f028e32619febe9f15f91fffa
Miguel Alonso <miguitarra59@gmail.com>
2025-06-10 09:47:46 +0200

    Generalized SCF routine + ERI Libint
    Changes: 
    - Usage of .xyz input files following quantum chemistry convention.
    - Basis sets are read by Fortran, the basis sets exponents and coefficients are held in the data/basis_set directory.
    - Usage of Libint for the calculation  of ERIs
    - OpenMP accelerated calls of Libint
    - OpenACC is relegated to the Fock matrix building and SCF cycle. 
    
    
    To come:
    - Usage of Libint for the calculation of one-electron integrals (overlap, kinetic, and nuclear potential) through a C++ wrapper.
    - Benchmarking.

--- src/c/scf_interface.c
+++ src/c/scf_interface.c
@@ -3,13 +3,13 @@
 #include <stdint.h>
 
 // Fortran subroutine interface
-void run_scf_interface_py(int32_t *natoms, int32_t *nelectrons, int32_t *nalpha, int32_t *nbeta,int32_t *atomic_numbers, double *coords, 
-                          double *eps_scf, int32_t *max_iter, double *final_energy, 
-                          int32_t *nbasis_atom, double *basis_exp, int32_t *basis_set);
+void run_scf_interface_py(int32_t *natoms, int32_t *nelectrons, int32_t *nalpha, int32_t *nbeta,
+                          const char *species, double *coords, double *eps_scf, int32_t *max_iter,
+                          double *final_energy, int32_t *basis_set_n, int32_t *z_num);
 
-void run_scf_c(int natoms, int nelectrons, int nalpha, int nbeta, int *atomic_numbers, double *coords, double eps_scf, 
-               int max_iter, double *final_energy, int *nbasis_atom, double *basis_exp, int basis_set) {
-    // Call the Fortran subroutine
-    run_scf_interface_py(&natoms, &nelectrons, &nalpha, &nbeta, atomic_numbers, coords, &eps_scf, &max_iter, 
-                         final_energy, nbasis_atom, basis_exp, &basis_set);
+void run_scf_c(int natoms, int nelectrons, int nalpha, int nbeta, const char *species, double *coords,
+               double eps_scf, int max_iter, double *final_energy, int32_t *basis_set_n, int32_t *z_num) {
+    run_scf_interface_py(&natoms, &nelectrons, &nalpha, &nbeta, species, coords,
+                         &eps_scf, &max_iter, final_energy, basis_set_n, z_num);
 }
+
